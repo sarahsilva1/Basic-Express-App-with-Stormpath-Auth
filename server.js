52e105b7fc706d9cd5a878520aaea242d523e2eb
Sarah <sarahsilva@live.com>
2016-11-14 11:39:58 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var stormpath = require('express-stormpath');
+
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.use(stormpath.init(app, {
+  expand: {
+    customData: true
+  }
+}));[]
+// not this yet
+app.get('/', stormpath.getUser, function(req, res) {
+  res.render('home', {
+    title: 'Welcome'
+  });
+});
+
+// not this yet
+
+app.use('/profile',stormpath.loginRequired,require('./profile')());
+
+
+app.on('stormpath.ready',function(){
+  console.log('Hey, Sarah, Stormpath is ready on port 3000.');
+});
+
+
+app.listen(3000);
